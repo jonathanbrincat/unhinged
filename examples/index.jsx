1b64e012cf322bba4b70ffe10ad0272fb1bc5c35
jonathan <jonathan@pix8.co.uk>
2023-10-23 14:45:38 +0100

    leftovers from react upgrade

--- examples/index.jsx
+++ examples/index.jsx
@@ -2,17 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// const render = Component => {
-//   ReactDOM.render(
-//     <AppContainer>
-//       <Component />
-//     </AppContainer>,
-//     document.getElementById('app'),
-//   )
-// }
-
-// render(App)
-
 ReactDOM
   .createRoot(document.getElementById('app'))
   .render(
@@ -20,8 +9,3 @@ ReactDOM
       <App />
     </React.StrictMode>,
   )
-
-// Webpack Hot Module Replacement API
-// if (module.hot) {
-//   module.hot.accept('./App', () => { render(App) })
-// }
