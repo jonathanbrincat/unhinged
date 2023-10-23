14ef87e3fed03ee7c420389e2cfb36665839dfdc
jonathan <jonathan@pix8.co.uk>
2023-10-23 23:43:01 +0100

    app heavily revised; the named smart wrapper component which I suspect perhaps aided the demo but otherwised appeared to serve the no purpose is removed

--- examples/App.jsx
+++ examples/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect }from 'react'
+import React, { useState }from 'react'
 import PivotTableUI from '../src/PivotTableUI'
 import TableRenderers from '../src/TableRenderers'
+import { aggregators } from '../src/Utilities'
 import createPlotlyComponent from 'react-plotly.js/factory'
 // import { Chart } from 'react-chartjs-2'
 import createPlotlyRenderers from '../src/PlotlyRenderers'
@@ -12,82 +13,43 @@ import '../src/pivottable.css'
 const PlotlyComponent = createPlotlyComponent(window.Plotly) // JB: create instance of Plotly
 // const ChartjsComponent = new Chart() // JB: create instance of chart.js
 
-class PivotTableUISmartWrapper extends React.PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pivotState: props
-    }
-  }
-
-  // JB: why is this even necessary? he sets props to state on initilisation in the constructor then schedules props to override state whenever they are changed. 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    console.log('dirty south => ', nextProps)
-
-    this.setState({pivotState: nextProps})
-  }
-
-  render() {
-    return (
-      <PivotTableUI
-        renderers={Object.assign(
-          {},
-          TableRenderers,
-          createPlotlyRenderers(PlotlyComponent),
-          createMyRenderers(),
-        )}
-        {...this.state.pivotState}
-        onChange={s => this.setState({pivotState: s})}
-      />
-    )
-  }
+const config = {
+  rows: ['Payer Gender'],
+  cols: ['Party Size'],
+  // aggregatorName: 'Sum over Sum',
+  vals: ['Tip', 'Total Bill'],
+  rendererName: 'Grouped Column Chart',
+  sorters: {
+    Meal: sortAs(['Lunch', 'Dinner']),
+    'Day of Week': sortAs([
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+    ]),
+  },
+  plotlyOptions: { width: 900, height: 500 },
+  plotlyConfig: {},
+  tableOptions: {},
 }
 
-export default function App() {
-  // JB: this doesn't even need to be state. It's all static configuration.
-  // const [ pivotState, setPivotState ] = useState({
-  //   data: tips,
-  //   rows: ['Payer Gender'],
-  //   cols: ['Party Size'],
-  //   // aggregatorName: 'Sum over Sum',
-  //   vals: ['Tip', 'Total Bill'],
-  //   rendererName: 'Grouped Column Chart',
-  //   sorters: {
-  //     Meal: sortAs(['Lunch', 'Dinner']),
-  //     'Day of Week': sortAs([
-  //       'Thursday',
-  //       'Friday',
-  //       'Saturday',
-  //       'Sunday',
-  //     ]),
-  //   },
-  //   plotlyOptions: { width: 900, height: 500 },
-  //   plotlyConfig: {},
-  //   tableOptions: {},
-  // })
-
-  const options = {
-    data: tips,
-    rows: ['Payer Gender'],
-    cols: ['Party Size'],
-    // aggregatorName: 'Sum over Sum',
-    vals: ['Tip', 'Total Bill'],
-    rendererName: 'Grouped Column Chart',
-    sorters: {
-      Meal: sortAs(['Lunch', 'Dinner']),
-      'Day of Week': sortAs([
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday',
-      ]),
-    },
-    plotlyOptions: { width: 900, height: 500 },
-    plotlyConfig: {},
-    tableOptions: {},
-  }
+export default function App(props) {
+  const [data, setData] = useState(tips)
+  const [options, setOptions] = useState(config)
 
   return (
-    <PivotTableUISmartWrapper {...options} />
+    <PivotTableUI
+      data={data}
+      renderers={{
+        ...TableRenderers,
+        ...createPlotlyRenderers(PlotlyComponent),
+        ...createMyRenderers(),
+      }}
+      aggregators={{
+        ...aggregators,
+      }}
+      {...options}
+      onChange={state => setOptions(state)}
+    />
   )
 }
