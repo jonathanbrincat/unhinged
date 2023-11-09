e2bd73e8e75be41e75b0fbfa4a558d0071c5e2d1
jonathan <jonathan@pix8.co.uk>
2023-11-09 22:03:35 +0000

    removal of redundant code from app and refinement of table styles

--- examples/archive/App.prop-recirculation.jsx
+++ examples/archive/App.prop-recirculation.jsx
@@ -34,42 +34,54 @@ const options = {
   tableOptions: {},
   unusedOrientationCutoff: Infinity,
 
+  // JB: undocumented feature - specify filters
   valueFilter: {
     'Payer Gender': { 'Male': true },
   },
 }
 
-// JB: there really needs to be a way to declare a primary_key field in the imported csv data; for 2 reasons. 1) to allow custom aggregators to known what field is the unique identifier 2) so that the primary_key can be excluded from the available dimensions.
-
 export default function App(props) {
   const [data, setData] = useState(MOCK)
+  // const [pivotState, setPivotState] = useState({})
 
-  useEffect(() => {
-    fetch(API)
-      .then((response) => response.text())
-      .then((csv) => {
-        Papa.parse(csv, {
-          complete: (parsed) => setData(parsed.data),
-          error: (error) => console.log(error),
-        })
-      })
-      .catch((error) => {
-        console.log('Something went wrong retrieving the csv data from Google Docs :: ', error)
-      })
-  }, [])
+  // useEffect(() => {
+  //   fetch(API)
+  //     .then((response) => response.text())
+  //     .then((csv) => {
+  //       Papa.parse(csv, {
+  //         complete: (parsed) => setData(parsed.data),
+  //         error: (error) => console.log(error),
+  //       })
+  //     })
+  //     .catch((error) => {
+  //       console.log('Something went wrong retrieving the csv data from Google Docs :: ', error)
+  //     })
+  // }, [])
 
   return (
-    <PivotTableUI
-      data={data} // REQUIRED - everything else is optional
-      renderers={{
-        ...TableRenderers,
-        ...createPlotlyRenderers(PlotlyComponent),
-        ...createChartjsRenderers(),
-      }}
-      aggregators={{
-        ...aggregators,
-      }}
-      {...options}
-    />
+    <>
+      <p>Dataset</p>
+      <pre style={{ fontSize: '10px' }}>{JSON.stringify(data)}</pre>
+
+      {/* there really needs to be a way to declare a primary_key field in the imported csv data; for 2 reasons. 1) to allow custom aggregators to known what field is the unique identifier 2) so that the primary_key can be excluded from the available dimensions., */}
+      <PivotTableUI
+        data={data} // REQUIRED - everything else is optional
+        renderers={{
+          ...TableRenderers,
+          ...createPlotlyRenderers(PlotlyComponent),
+          ...createChartjsRenderers(),
+        }}
+        aggregators={{
+          ...aggregators,
+        }}
+        {...options}
+        
+        // JB: REMOVE
+        // onChange={(state) => {
+        //   setPivotState(state)
+        // }}
+        // {...pivotState}
+      />
+    </>
   )
 }
